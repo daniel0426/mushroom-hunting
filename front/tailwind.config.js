5abee8a00b436a312281e5f554359f73b8580e0a
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-08-20 17:27:00 +1200

    Added in tailwind config

---
+++ front/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'fungi-files': {
+          'light-green': '#ADD4A4',
+          'dark-green': '#20431C',
+          'brown': '#8C7662',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
