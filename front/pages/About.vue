1fbb47a2af956f1129b3733abc0399fa6df7b96d
daniel0426 <angus9209@gmail.com>
2021-08-23 09:44:57 +1200

    amend About.vue

--- front/pages/About.vue
+++ front/pages/About.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <h1>About</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex est
-      dolor sed, rerum, sit consequatur tempore quidem nisi nam ad cum ab
-      similique explicabo ipsam aspernatur, in non quasi! Commodi, ab error
-      minima excepturi vero amet quos iusto nisi at quibusdam, harum aliquid
-      doloremque nesciunt minus exercitationem in fugit similique. Dolore
-      aperiam necessitatibus non quidem et perferendis quam sequi.
+    <h1 class="text-center text-2xl mb-12">About</h1>
+    <p class="w-4/6 m-auto mb-12">
+      Welcome to the Mushroom Hungting Blog!
+      <br>
+      This blog keeps you up-to-date with all the exciting additions to thie website.
+      You can upload your beautiful mushrooms and can share with various people who are passionate about mushroom.
+      Show your Passion!
     </p>
+    <div class="imageContainer w-4/6  border-black m-auto grid grid-cols-1 gap-6 md:grid-cols-2">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1562955239-78d8c23a3542?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80" alt="mushroomImg" data-aos="fade-right">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1351&q=80" alt="mushroomImg">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1519305124423-5ccccff55da9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="mushroomImg">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1602860635077-d67e70469097?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="mushroomImg">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1512595765784-5ebad80772a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80" alt="mushroomImg">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1550824730-05ededc35e7a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1190&q=80" alt="mushroomImg">
+      <img class="rounded shadow-lg transform transition duration-500 hover:scale-105" src="https://images.unsplash.com/photo-1454425064867-5ba516caf601?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="mushroomImg">
+
+      
+    </div>
   </div>
 </template>
 
