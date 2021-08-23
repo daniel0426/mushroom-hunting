ed8da0e12a3d7a15b0b728a4f9e4e738e638fd9c
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-08-23 19:32:23 +1200

    Added view blog detail functionality and restructured posts folder structure

--- front/components/Blog.vue
+++ front/components/Blog.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="rounded-md overflow-hidden">
-    <h3>{{blog.title}}</h3>
-    <img class="object-cover w-full h-full" :src="blog.imgURL" alt="post image" />
-    <div class="absolute">
-      <div class="blog-summ flex flew-row justify-between border border-indigo-500">
-        <ul class="mr-30">
-          <li v-for="tag in blog.tags" :key="tag.id"> 
-            <h3>{{tag}}</h3>
-          </li>
-        </ul>
-        <button >See Detail</button>
-      </div>
+  <div class="rounded-md ">
+    <h3>{{ blog.title }}</h3>
+    <img
+      class="object-cover w-full h-full"
+      :src="blog.imgURL"
+      alt="post image"
+    />
+    <div class="flex flex-row w-full justify-between z-10">
+      <ul class="mr-30">
+        <li v-for="tag in blog.tags" :key="tag.id">
+          <h3>{{ tag }}</h3>
+        </li>
+      </ul>
 
-      
+      <nuxt-link
+        :to="{
+          name: 'posts-id',
+          params: {
+            id: blog._id,
+          },
+        }"
+      >
+        Detail View
+      </nuxt-link>
     </div>
-
   </div>
-  <!-- <li class="col-span-1 flex flex-col text-center bg-white">
-    <div class="pb-full relative">
-      <div class="absolute inset-0 h-full w-full">
-        <img class="h-full w-full object-cover object-center" :src="mushroom.url" />
-      </div>
-    </div>
-  </li> -->
 </template>
 
 <script>
 export default {
   props: {
-    'blog': Object
+    blog: Object,
   },
 };
 </script>
 
 <style>
-
 </style>
