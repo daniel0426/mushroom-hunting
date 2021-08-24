2e29b695fcefd57cc37befb0d8e46f2a330f3521
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-08-24 21:33:50 +1200

    Added single tag component and refactored pages accordingly.

---
+++ front/components/SingleTag.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="
+      p-3
+      bg-fungi-files-light-green
+      text-fungi-files-dark-green
+      rounded-lg
+      m-2
+      text-lg
+    "
+  >
+    {{ tag }}
+  </button>
+</template>
+
+<script>
+export default {
+    props: {
+        tag: String,
+    }
+};
+</script>
+
+<style>
+</style>
