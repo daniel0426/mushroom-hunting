63039f29ca41ba490e74e2f7041e97cb3e3f8b23
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-08-24 20:28:19 +1200

    Added padding for medium screen size, improved responsive

--- front/layouts/app.vue
+++ front/layouts/app.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex flex-col">
     <Nav />
 
-    <div class="flex flex-grow overflow-y-auto py-10">
+    <div class="flex flex-grow overflow-y-auto px-10 md:px-4 py-8">
       <nuxt class="md:max-w-7xl mx-auto" />
     </div>
 
