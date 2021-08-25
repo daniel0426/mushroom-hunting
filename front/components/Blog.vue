71c0f247e5e1fb1e599ad173eed50111247daf25
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-08-25 18:34:10 +1200

    Formatting

--- front/components/Blog.vue
+++ front/components/Blog.vue
@@ -7,21 +7,30 @@
       alt="post image"
     />
     <div class="flex flex-row w-full justify-between z-10">
-        <SingleTag v-for="(tag, index) in blog.tags" :key="index" :tag="tag" />
+      <SingleTag v-for="(tag, index) in blog.tags" :key="index" :tag="tag" />
 
       <nuxt-link
-      
         :to="{
           name: 'blogs-id',
           params: {
             id: blog._id,
           },
         }"
-        
       >
-        <button class="px-3 py-1 bg-gray-400 text-white rounded-lg m-2 text-md cursor-pointer">
+        <button
+          class="
+            px-3
+            py-1
+            bg-gray-400
+            text-white
+            rounded-lg
+            m-2
+            text-md
+            cursor-pointer
+          "
+        >
           Detail View
-          </button>
+        </button>
       </nuxt-link>
     </div>
   </div>
